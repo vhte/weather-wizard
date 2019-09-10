1be314152fd64c80a3c96aca0705ec7ad463611d
Victor Torres <talk@victortorr.es>
2019-09-09 20:27:16 -0400

    Transfered the weather wizard as the interface to the user

--- api.py
+++ api.py
@@ -1,13 +1,7 @@
-# from weatherwizard import WeatherWizard
-from openweather import OpenWeatherMap, OpenWeatherException
+from weatherwizard import WeatherWizard
 from neural import NeuralNetwork
 
-try:
-    ow = OpenWeatherMap()
-    # 6325494 (Québec)
-    print(ow.action("forecast", 6325494))
-    # request.status_code / json()
-except OpenWeatherException as owe:
-    print("Error running OpenWeather: " + owe.get_message())
-finally:
-    print("--- End of execution --")
+ww = WeatherWizard(6325494) # Québec
+print(ww.tomorrow_forecast())
+print("--- End of execution --")
+
--- weatherwizard.py
+++ weatherwizard.py
@@ -2,5 +2,30 @@ from openweather import OpenWeatherMap, OpenWeatherException
 
 
 class WeatherWizard:
-    def __init__(self):
+    def __init__(self, city):
         self.__ow = OpenWeatherMap()
+        self.__last_response = {"cod": 0, "response": ""}
+        self.__city = city
+
+    def tomorrow_forecast(self):
+        """
+        Abstracts a human readable forecast for the next day
+
+        Args
+
+        Raises
+            OpenWeatherException: A unexpected behavior when calling OpenWeather API
+        Returns
+            __last_response: Dictionary in format {"cod": (int), "response": (str)}
+        """
+        try:
+            forecast = self.__ow.action("forecast", self.__city)
+            #TODO apply intelligence
+            self.__last_response = forecast
+            # request.status_code / json()
+        except OpenWeatherException as owe:
+            self.__last_response.cod = -1
+            self.__last_response.response = "Error running OpenWeather: " + owe.get_message()
+
+        return self.__last_response
+
