c1bd34f40967cc3d4f0c7ec43ac57c4b1ff22252
Victor Torres <talk@victortorr.es>
2019-09-10 20:54:59 -0400

    Drawing some ideas for the neural network, added private static as helper

--- neural.py
+++ neural.py
@@ -1,3 +1,13 @@
 class NeuralNetwork:
+    """
+    NeuralNetwork
+
+    Temperature[k]|Humidity[%]|Temp(max/min)|Wind[m/s]|Rain[mm]|Snow[mm]|Cloud[%](?)|Pressure[hPa](?)
+    """
+    VEHICLES = ["walk", "run", "bike", "bus", "car", "boat"]
+
     def __init__(self):
+        pass
+
+    def classify(self, vehicle):
         pass
--- weatherwizard.py
+++ weatherwizard.py
@@ -1,5 +1,5 @@
 from openweather import OpenWeatherMap, OpenWeatherException
-
+from neural import NeuralNetwork
 
 class WeatherWizard:
     def __init__(self, city):
@@ -7,9 +7,9 @@ class WeatherWizard:
         self.__last_response = {"cod": 0, "response": ""}
         self.__city = city
 
-    def tomorrow_forecast(self):
+    def weather(self):
         """
-        Abstracts a human readable forecast for the next day
+        Abstracts a human readable weather for the current time
 
         Args
 
@@ -19,13 +19,20 @@ class WeatherWizard:
             __last_response: Dictionary in format {"cod": (int), "response": (str)}
         """
         try:
-            forecast = self.__ow.action("forecast", self.__city)
-            #TODO apply intelligence
-            self.__last_response = forecast
+            neural = NeuralNetwork()
             # request.status_code / json()
+            self.__last_response = neural.classify("walk", self.__ow.action("weather", self.__city))
         except OpenWeatherException as owe:
             self.__last_response.cod = -1
             self.__last_response.response = "Error running OpenWeather: " + owe.get_message()
 
         return self.__last_response
+
+    @classmethod
+    def kelvin_to_celsius(cls, temperature):
+        return temperature - 273.15
+
+    @classmethod
+    def ms_to_kmh(cls, speed):
+        return speed * 3.6
 
