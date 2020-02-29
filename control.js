bf0cf71b47f25bf16735b8fe0e5219ba6bda52be
Victor Torres <talk@victortorr.es>
2020-02-29 15:52:10 -0500

    Added promise and json response to xhr

--- control.js
+++ control.js
@@ -1,8 +1,19 @@
 function get_weather_data(cities_ids) {
-    const XHR = new XMLHttpRequest();
-    XHR.open("POST", "http://localhost:8000/")
-    XHR.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    return XHR.send(encodeURIComponent(cities_ids))
+    return new Promise((resolve, reject) => {
+        const XHR = new XMLHttpRequest();
+        XHR.open("POST", "http://localhost:8000/");
+        XHR.setRequestHeader("Content-type", "application/json");
+        //return XHR.send(encodeURIComponent(cities_ids))
+        XHR.onload = function(e) {
+            if(XHR.readyState === 4)
+                if(XHR.status === 200)
+                    resolve(JSON.parse(XHR.responseText));
+                else
+                    console.error(XHR.statusText);
+                    reject(XHR.statusText);
+        }
+        XHR.send(JSON.stringify(cities_ids));
+    });
 }
 
 window.addEventListener('DOMContentLoaded', (event) => {
@@ -23,15 +34,17 @@ window.addEventListener('DOMContentLoaded', (event) => {
                 return response.json();
             }).then((json) => {
                 // POST
-                post_result = get_weather_data(json["countries"][country.value].map(city => city[0]))
-                console.log(post_result)
+                get_weather_data(json["countries"][country.value].map(city => city[0])).then((post_result) => {
+                    console.log(post_result);
+                });
+
 
                 // Enumerate cities
                 for(let i = 0;i < json["countries"][country.value].length;i++) {
                     let elem = document.createElement("li");
                     elem.appendChild(document.createTextNode(json["countries"][country.value][i][1] + ": TODO"));
                     cities.appendChild(elem);
-                };
+                }
             });
 
         });
--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import http.server
 import json
-from urllib import parse
+from weatherwizard import WeatherWizard
 
 
 class Handler(http.server.BaseHTTPRequestHandler):
@@ -8,20 +8,39 @@ class Handler(http.server.BaseHTTPRequestHandler):
 
     def header(self):
         self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.send_header("Access-Control-Allow-Origin", "*")  # For cross POST using different ports
+        self.send_header("Content-type", "application/json")
+        self.send_header("Access-Control-Allow-Origin", "*")
         self.end_headers()
 
     def do_GET(self):
         self.header()
-        self.wfile.write(b"<html><body><h1>Accepting only POST</h1></body></html>")
+        self.wfile.write(b"<html><body><h1>Accepting only POST in /</h1></body></html>")
+
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
+        self.send_header("Access-Control-Allow-Headers", "Content-Type")
+        self.end_headers()
 
     def do_POST(self):
         self.header()
         content_length = int(self.headers["Content-Length"])
         post_data = self.rfile.read(content_length)
-        post_data = parse.parse_qs(post_data.decode(self.__encoding))
-        self.wfile.write(bytes(json.dumps(post_data), self.__encoding))
+        print("Original post data: {}".format(post_data))
+        # str bytes to list
+        cities_ids = post_data.decode('utf-8').strip("[]").split(",")
+        # post_data = parse.parse_qs(parse.unquote(post_data.decode(self.__encoding)))
+        print("Transformed post data: {}".format(cities_ids))
+
+        ww = WeatherWizard()
+        result = []
+        for city in cities_ids:
+            ww.set_city(city)
+            result.append(ww.weather())
+        print(result)
+
+        self.wfile.write(bytes(json.dumps(result), self.__encoding))
         # r = requests.post("http://localhost:8000/", data={'foo': 'bar', 'weather': 'wizard'}) r.text
 
 
